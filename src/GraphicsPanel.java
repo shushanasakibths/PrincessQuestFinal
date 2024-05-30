78d54b62b559203ec167416f7bea3f2741a35e04
BT_1N3_33 <sample@bths.edu>
2024-05-30 10:57:25 -0400

    day7

---
+++ src/GraphicsPanel.java
@@ -0,0 +1,96 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GraphicsPanel extends JPanel implements KeyListener, MouseListener, ActionListener {
+    private BufferedImage background;
+    private Player player;
+    private boolean[] pressedKeys;
+
+
+    public GraphicsPanel(String name) {
+        try {
+            background = ImageIO.read(new File("src/background.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        player = new Player("src/Princess/Idle/IdleRight.png", name);
+        pressedKeys = new boolean[128];
+        addKeyListener(this);
+        addMouseListener(this);
+        setFocusable(true);
+        requestFocusInWindow();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(player.getPlayerImage(), player.getxCoord(), player.getyCoord(), player.getWidth(), player.getHeight(), null);
+
+        if (pressedKeys[65]) {
+            player.faceLeft();
+            player.moveLeft();
+        }
+
+        // player moves right (D)
+        if (pressedKeys[68]) {
+            player.faceRight();
+            player.moveRight();
+        }
+
+        // player moves up (W)
+        if (pressedKeys[87]) {
+            player.moveUp();
+        }
+
+        // player moves down (S)
+        if (pressedKeys[83]) {
+            player.moveDown();
+        }
+    }
+
+    // ----- KeyListener interface methods -----
+    public void keyTyped(KeyEvent e) { } // unimplemented
+
+    public void keyPressed(KeyEvent e) {
+        // see this for all keycodes: https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
+        // A = 65, D = 68, S = 83, W = 87, left = 37, up = 38, right = 39, down = 40, space = 32, enter = 10
+        int key = e.getKeyCode();
+        pressedKeys[key] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        pressedKeys[key] = false;
+    }
+
+    // ----- MouseListener interface methods -----
+    public void mouseClicked(MouseEvent e) { }  // unimplemented; if you move your mouse while clicking,
+
+    public void mousePressed(MouseEvent e) { } // unimplemented
+
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {  // left mouse click
+            Point mouseClickLocation = e.getPoint();
+        } else {
+            Point mouseClickLocation = e.getPoint();
+            if (player.playerRect().contains(mouseClickLocation)) {
+                player.turn();
+            }
+        }
+    }
+
+    public void mouseEntered(MouseEvent e) { } // unimplemented
+
+    public void mouseExited(MouseEvent e) { } // unimplemented
+
+    // ACTIONLISTENER INTERFACE METHODS: used for buttons AND timers!
+    public void actionPerformed(ActionEvent e) {
+    }
+}
